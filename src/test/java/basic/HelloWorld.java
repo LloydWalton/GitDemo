d2481f72a1e6ef350b2a95ffb2d21ca467741b1b
Lloyd Walton <c.lloydwalton@gmail.com>
2022-11-05 20:50:04 +0530

    First Commit

---
+++ src/test/java/basic/HelloWorld.java
@@ -0,0 +1,17 @@
+package basic;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+	
+	
+	@Test
+	public void testcase()
+	{
+		
+		System.out.println("Hello World");
+		
+		System.out.println("This is for testing purpose");
+	}
+
+}
