80c766e5dbab29d3cececeb10c37ce6576a97b41
Lloyd Walton <c.lloydwalton@gmail.com>
2022-11-05 22:36:32 +0530

    Removed code

--- src/test/java/basic/NewFile.java
+++ src/test/java/basic/NewFile.java
@@ -5,7 +5,7 @@ public class NewFile {
 	public static void main(String[] args) {
 		System.out.println("New File");
 		
-		System.out.println("Develop  branch");
+		System.out.println("Develop");
 	}
 
 }
