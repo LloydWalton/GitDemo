5fbbc97044cf5c61c036c4e5574337e092391572
Lloyd Walton <c.lloydwalton@gmail.com>
2022-11-06 11:03:54 +0530

    clone

--- src/test/java/basic/NewFile.java
+++ src/test/java/basic/NewFile.java
@@ -5,7 +5,6 @@ public class NewFile {
 	public static void main(String[] args) {
 		System.out.println("New File");
 		System.out.println("New File clone");
-
 		System.out.println("New File2");
 	}
 
